7574fdd93b360d0dd6acdc68ba785cad38119bbe
nguy2261 <nguy2261@umn.edu>
2015-06-08 21:37:58 -0500

    Update ScrapeAirbnb.py

--- ScrapeAirbnb.py
+++ ScrapeAirbnb.py
@@ -740,6 +740,35 @@ def getAmenities(tree, ListingID):
     
 ######################################
 #### Save Results ####################
+
+class DictUnicodeWriter(object):
+
+    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, D):
+        self.writer.writerow({k:v.encode("utf-8") if isinstance(v, unicode) else v for k,v in D.items()})
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for D in rows:
+            self.writerow(D)
+
+    def writeheader(self):
+        self.writer.writeheader()
+
 def writeToCSV(resultDict, outfile):
     
     colnames = [ 'ListingID', 'Title','UserID','baseurl',  'Price', \
@@ -757,26 +786,9 @@ def writeToCSV(resultDict, outfile):
          'S_CheckIn','S_Checkout','S_NumBeds','S_PropType','ShortDesc']
         
     with open(outfile, 'wb') as f:
-        w = csv.DictWriter(f, fieldnames=colnames)
+        w = DictUnicodeWriter(f, fieldnames=colnames)
         w.writeheader()
-        w.writerows([clean_dict(v) for v in resultDict])
-
-def clean_dict(obj):
-    """
-    use the clean() function to clean all values in the dictionary of 
-    unwanted characters
-    """
-    return {k:clean(v) for (k,v) in obj.items()}
-
-def clean(stuff):
-    """This is a cheap way of getting rid of some unicode characters that are
-    causing some problems"""
-    
-    if type(stuff) == unicode:
-        #TODO:  Use Regex instead of this
-        return stuff.replace(u'\xb7', '').replace(u'\u2022', '').replace(' ', '')
-        
-    return stuff
+        w.writerows(resultDict)
 
 #######################################
 #  Testing ############################
@@ -789,9 +801,9 @@ if __name__ == '__main__':
     
     #Take The Main Results From Previous Step and Iterate Through Each Listing
     #To add more detail
-    #DetailResults = iterateDetail(MainResults)
+    DetailResults = iterateDetail(MainResults)
     
     #Write Out Results To CSV File, using function I defined
-    writeToCSV(MainResults, 'OtherWriter.csv')
+    writeToCSV(DetailResults, 'OtherWriter.csv')
     
     
